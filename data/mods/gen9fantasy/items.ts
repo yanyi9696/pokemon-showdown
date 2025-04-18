5591344ecffbdfa28b63e43405715352318e316d
Nihilslave <ApclpsZm90@hotmail.com>
2025-04-19 00:18:48 +0800

    add items for gen9fantasy

---
+++ data/mods/gen9fantasy/items.ts
@@ -0,0 +1,17 @@
+export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
+	fantasyitem: {
+		name: "Fantasy Item",
+		spritenum: 523,
+		fling: {
+			basePower: 30,
+		},
+		onBasePowerPriority: 15,
+		onBasePower(basePower, user, target, move) {
+			if (move.type === 'Stellar') {
+				return this.chainModify([4915, 4096]);
+			}
+		},
+		num: 10000,
+		gen: 9,
+	},
+};
