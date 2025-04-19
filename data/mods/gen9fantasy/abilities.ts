a368901656b7fd07a73d9beec8596660abf7cee6
Anglos <2448172102@qq.com>
2025-04-19 21:07:17 +0800

    蜂女王特性重做，定级

--- data/mods/gen9fantasy/abilities.ts
+++ data/mods/gen9fantasy/abilities.ts
@@ -1,19 +1,20 @@
 export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTable = {
-	fengchaofanghu: {
-		onDamage(damage, target, source, effect) {
-			if (effect.effectType !== 'Move') {
-				if (effect.effectType === 'Ability') this.add('-activate', source, 'ability: ' + effect.name);
-				return false;
+	fengqunshouhu: {
+		onResidual(pokemon) {
+			if (pokemon.hp && pokemon.status && this.randomChance(50, 100)) {
+				this.debug('蜂群守护');
+				this.add('-activate', pokemon, 'ability: 蜂群守护');
+				pokemon.cureStatus();
 			}
 		},
 		onEffectivenessPriority: -1,
 		onEffectiveness(typeMod, target, type, move) {
 			if (move && move.effectType === 'Move' && move.category !== 'Status' && type === 'Bug' && typeMod > 0) {
-				this.add('-activate', target, 'ability: Fengchao');
+				this.add('-activate', target, 'ability: 蜂群守护');
 				return 0;
 			}
 		},
-		name: "蜂巢守护",
+		name: "蜂群守护",
 		rating: 3.5,
 		num: 10001,
 		shortDesc: "虫属性的弱点消失，不会受到攻击以外的伤害。",
--- data/mods/gen9fantasy/formats-data.ts
+++ data/mods/gen9fantasy/formats-data.ts
@@ -2,6 +2,12 @@ export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormat
 	garchompfantasy: {
 		tier: "OU",
 	},
+	garchompmegafantasy: {
+		tier: "(Uber)",
+	},
+	vespiquenfantasy: {
+		tier: "(Uber)",
+	},
 	//上为幻想，下为分级变动
 	shedinja: {
 		tier: "AG",
