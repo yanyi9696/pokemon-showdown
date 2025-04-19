1f636656c608022eefa83545b6a8f739142ce6f1
Anglos <2448172102@qq.com>
2025-04-19 16:41:25 +0800

    增加幻想Mega地龙

--- data/mods/gen9fantasy/moves.ts
+++ data/mods/gen9fantasy/moves.ts
@@ -22,4 +22,26 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 		desc: "Example New Move for Gen 9 Fantasy. Has a 100% chance to raise the user's Special Attack by 1 stage.",
 		shortDesc: "Example Move. 100% chance to raise the user's Sp. Atk by 1."
 	},
+	xianxing: {
+		num: 10001,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		name: "Xianxing",
+		pp: 25,
+		priority: 0,
+		flags: { protect: 1, mirror: 1, metronome: 1 },
+		volatileStatus: 'xianxing',
+		condition: {
+			onFractionalPriorityPriority: -2,
+			onFractionalPriority(priority, pokemon) {
+				if (priority <= 0) return 0.1;
+			},
+		},
+		target: "normal",
+		type: "Bug",
+		contestType: "Clever",
+		desc: "使用之后技能会优先出手",
+		shortDesc: "使用之后技能会优先出手"
+	}
 };
