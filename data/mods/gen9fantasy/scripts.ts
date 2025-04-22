e18b11538904598d9f4398017c7ee71584e436d0
Anglos <2448172102@qq.com>
2025-04-22 22:13:35 +0800

    ubersuu名单有误，尝试修改

--- data/mods/gen9fantasy/scripts.ts
+++ data/mods/gen9fantasy/scripts.ts
@@ -5,6 +5,7 @@ export const Scripts: ModdedBattleScriptsData = {
 			if (this.data.FormatsData[id].isNonstandard === 'Past') delete this.data.FormatsData[id].isNonstandard;
 			if (this.data.FormatsData[id].natDexTier) {
 				this.data.FormatsData[id].tier = this.data.FormatsData[id].natDexTier;
+				delete this.data.FormatsData[id].natDexTier;
 			}
 		}
 	}
