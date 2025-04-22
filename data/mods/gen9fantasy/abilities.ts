89e9dde5f58fe691e312820aaeb82c41206d8ce1
Anglos <2448172102@qq.com>
2025-04-23 00:15:35 +0800

    蜂巢特性效果重做

--- data/mods/gen9fantasy/abilities.ts
+++ data/mods/gen9fantasy/abilities.ts
@@ -1,22 +1,35 @@
 export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTable = {
 	fengchao: {
-		onDamage(damage, target, source, effect) {
-			if (effect.effectType !== 'Move') {
-				if (effect.effectType === 'Ability') this.add('-activate', source, 'ability: ' + effect.name);
-				return false;
-			}
-		},
 		onEffectivenessPriority: -1,
 		onEffectiveness(typeMod, target, type, move) {
 			if (move && move.effectType === 'Move' && move.category !== 'Status' && type === 'Bug' && typeMod > 0) {
 				this.add('-activate', target, 'ability: Fengchao');
-				return 0;
+				return 0; // Bug-type attacks no longer have effectiveness changes
+			}
+		},
+		onModifyAtkPriority: 5,
+		onModifyAtk(atk, attacker, defender, move) {
+			if (move.type === 'Bug') {
+				this.debug('Fengchao Bug move power boost');
+				return this.chainModify(1.5); // Increase Bug-type move attack power by 1.5x
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA(atk, attacker, defender, move) {
+			if (move.type === 'Bug') {
+				this.debug('Fengchao Bug move special attack boost');
+				return this.chainModify(1.5); // Increase Bug-type move special attack power by 1.5x
+			}
+		},
+		onAfterMove(pokemon, target, move) {
+			if (move.type === 'Bug') {
+				this.heal(pokemon.baseMaxhp / 8); // Heal 1/8 of max HP after using Bug-type move
 			}
 		},
 		name: "Fengchao",
 		rating: 3.5,
 		num: 10001,
-		shortDesc: "虫属性的弱点消失，不会受到攻击以外的伤害。",
+		shortDesc: "虫属性的弱点消失。虫属性招式威力提升1.5倍,使用虫属性招式时会回复最大HP的1/8。",	
 	},
 	stancechange: {
 		onModifyMovePriority: 1,
