9b44f63cec1013ce3b4aa640a8678d4683461dbb
Anglos <2448172102@qq.com>
2025-06-12 17:45:31 +0800

    二次修坚盾剑怪bug

--- data/abilities.ts
+++ data/abilities.ts
@@ -4437,22 +4437,22 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 	},
 	stancechange: {
 		onModifyMove(move, attacker, defender) {
-		// 不是 Aegislash 系列 或者 变身状态的，跳过
-		if (!['Aegislash', 'Aegislash-Fantasy'].includes(attacker.species.baseSpecies) || attacker.transformed) return;
+			// 不是 Aegislash 系列 或者 变身状态的，跳过
+			if (!['Aegislash', 'Aegislash-Blade', 'Aegislash-Fantasy', 'Aegislash-Blade-Fantasy'].includes(attacker.species.name) || attacker.transformed) return;
 
-		// 如果是变化招式且不是 King's Shield，跳过
-		if (move.category === 'Status' && move.id !== 'kingsshield') return;
+			// 如果是变化招式且不是 King's Shield，跳过
+			if (move.category === 'Status' && move.id !== 'kingshield') return;
 
-		// 确定目标形态
-		let targetForme;
-		if (move.id === 'kingshield') {
-			targetForme = attacker.species.baseSpecies === 'Aegislash' ? 'Aegislash' : 'Aegislash-Fantasy';
-		} else {
-			targetForme = attacker.species.baseSpecies === 'Aegislash' ? 'Aegislash-Blade' : 'Aegislash-Blade-Fantasy';
-		}
+			// 确定目标形态
+			let targetForme;
+			if (move.id === 'kingshield') {
+				targetForme = attacker.species.name.includes('Fantasy') ? 'Aegislash-Fantasy' : 'Aegislash';
+			} else {
+				targetForme = attacker.species.name.includes('Fantasy') ? 'Aegislash-Blade-Fantasy' : 'Aegislash-Blade';
+			}
 
-		// 如果当前形态和目标形态不同，切换形态
-		if (attacker.species.name !== targetForme) attacker.formeChange(targetForme);
+			// 切换形态
+			if (attacker.species.name !== targetForme) attacker.formeChange(targetForme);
 		},
 		flags: { failroleplay: 1, noreceiver: 1, noentrain: 1, notrace: 1, failskillswap: 1, cantsuppress: 1 },
 		name: "Stance Change",
