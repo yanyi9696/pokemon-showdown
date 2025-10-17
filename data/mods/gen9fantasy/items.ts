adc6c8e756b994b8a2ddea24c14fcb80eddb9d68
Anglos <2448172102@qq.com>
2025-10-17 19:08:46 +0800

    迷糊了，名字打错了

--- data/mods/gen9fantasy/items.ts
+++ data/mods/gen9fantasy/items.ts
@@ -123,9 +123,9 @@ export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
 	fantasygreninjite: {
 		name: "Fantasy Greninjite",
 		spritenum: 585, 
-		megaStone: "Greninjite-Mega-Fantasy",
-		megaEvolves: "Greninjite",
-		itemUser: ["Greninjite-Fantasy"],
+		megaStone: "Greninja-Mega-Fantasy",
+		megaEvolves: "Greninja",
+		itemUser: ["Greninja-Fantasy"],
 		onTakeItem(item, source) {
 			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
 			return true;
