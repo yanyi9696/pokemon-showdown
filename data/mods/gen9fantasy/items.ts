a24c3e4b85e90a9622ddf3a0c4ce4e7880b3c5d1
Anglos <2448172102@qq.com>
2025-10-21 22:27:28 +0800

    制作mega老翁龙

--- data/mods/gen9fantasy/items.ts
+++ data/mods/gen9fantasy/items.ts
@@ -195,6 +195,21 @@ export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
 		desc: "布里卡隆超级石。让布里卡隆携带后,在战斗时就能进行超级进化",
 		shortDesc: "布里卡隆超级石。让布里卡隆携带后,在战斗时就能进行超级进化",
 	},
+	drampanite: {
+		name: "Drampanite",
+		spritenum: 623, 
+		megaStone: "Drampa-Mega",
+		megaEvolves: "Drampa",
+		itemUser: ["Drampa"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: 10012, 
+		gen: 9, 
+		desc: "老翁龙超级石。让老翁龙携带后,在战斗时就能进行超级进化",
+		shortDesc: "老翁龙超级石。让老翁龙携带后,在战斗时就能进行超级进化",
+	},
 	//以下为Z num从20000开始
 	toxtricityz: {
 		name: "Toxtricity Z",
