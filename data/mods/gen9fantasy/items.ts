965cc05a55e2024d9e19f6d54a376e428e385939
Anglos <2448172102@qq.com>
2025-10-21 22:37:08 +0800

    制作mega列阵兵

--- data/mods/gen9fantasy/items.ts
+++ data/mods/gen9fantasy/items.ts
@@ -205,10 +205,25 @@ export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
 			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
 			return true;
 		},
-		num: 10012, 
+		num: 10013, 
 		gen: 9, 
 		desc: "老翁龙超级石。让老翁龙携带后,在战斗时就能进行超级进化",
 		shortDesc: "老翁龙超级石。让老翁龙携带后,在战斗时就能进行超级进化",
+	},
+	falinksite: {
+		name: "Falinksite",
+		spritenum: 602, 
+		megaStone: "Falinks-Mega",
+		megaEvolves: "Falinks",
+		itemUser: ["Falinks"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: 10014, 
+		gen: 9, 
+		desc: "列阵兵超级石。让列阵兵携带后,在战斗时就能进行超级进化",
+		shortDesc: "列阵兵超级石。让列阵兵携带后,在战斗时就能进行超级进化",
 	},
 	//以下为Z num从20000开始
 	toxtricityz: {
