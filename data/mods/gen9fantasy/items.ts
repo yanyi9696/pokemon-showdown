41b3a5f5bc2272e8a60f6591732bc9053f77e3b8
Anglos <2448172102@qq.com>
2025-10-23 17:28:03 +0800

    制作mega蜈蚣王

--- data/mods/gen9fantasy/items.ts
+++ data/mods/gen9fantasy/items.ts
@@ -360,6 +360,21 @@ export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
 		desc: "火炎狮超级石。让火炎狮携带后,在战斗时就能进行超级进化",
 		shortDesc: "火炎狮超级石。让火炎狮携带后,在战斗时就能进行超级进化",
 	},
+	scolipite: {
+		name: "Scolipite",
+		spritenum: 600, 
+		megaStone: "Scolipede-Mega",
+		megaEvolves: "Scolipede",
+		itemUser: ["Scolipede"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: 10023, 
+		gen: 9, 
+		desc: "蜈蚣王超级石。让蜈蚣王携带后,在战斗时就能进行超级进化",
+		shortDesc: "蜈蚣王超级石。让蜈蚣王携带后,在战斗时就能进行超级进化",
+	},
 	//以下为Z num从20000开始
 	toxtricityz: {
 		name: "Toxtricity Z",
