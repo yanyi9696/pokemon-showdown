3f93c229596396168b5fba7ba9c0a6bd5dfe7b83
Anglos <2448172102@qq.com>
2025-12-20 17:42:50 +0800

    进一步尝试

--- data/mods/gen9fantasy/items.ts
+++ data/mods/gen9fantasy/items.ts
@@ -690,7 +690,7 @@ export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
 		megaEvolves: "Flygon-Fantasy",
 		itemUser: ["Flygon-Fantasy"],
 		onTakeItem(item, source) {
-			if ([item.megaEvolves, item.megaStone].includes(source.baseSpecies.name)) return false;
+			if (item.megaEvolves!.includes(source.baseSpecies.name)) return false;
 			return true;
 		},
 		num: 10044, 
