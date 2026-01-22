ce836b181e2a29570ba55004310db074fa545433
Anglos <2448172102@qq.com>
2026-01-22 18:10:21 +0800

    将铸铠波分类为波动类技能

--- data/mods/gen9fantasy/moves.ts
+++ data/mods/gen9fantasy/moves.ts
@@ -837,7 +837,7 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 		name: "Zhu Kai Bo",
 		pp: 10,
 		priority: 0,
-		flags: { protect: 1, mirror: 1, heal: 1, metronome: 1 },
+		flags: { protect: 1, mirror: 1, heal: 1, metronome: 1, pulse: 1 },
 		drain: [1, 2],
 		secondary: null,
 		target: "normal",
