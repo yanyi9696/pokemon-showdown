bff2bf413c2e378b932dc2b55560a989bee3b8c3
Anglos <2448172102@qq.com>
2026-09-26 14:02:40 +0800

    闭关提升PP

--- data/mods/gen9fantasy/moves.ts
+++ data/mods/gen9fantasy/moves.ts
@@ -864,7 +864,7 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 		basePower: 0,
 		category: "Status",
 		name: "Shelter",
-		pp: 5,
+		pp: 10,
 		priority: 0,
 		flags: { snatch: 1, heal: 1, metronome: 1 },
 		heal: [1, 2],
